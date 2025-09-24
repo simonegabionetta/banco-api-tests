3780a2dcdb8c341885c80c82915030fdf9024fb0
Simone <smgabionetta@yahoo.com.br>
2025-09-24 10:30:14 +0100

    Adicionando o hook de beforeEach para captura do token

--- test/transferencias.test.js
+++ test/transferencias.test.js
@@ -1,15 +1,21 @@
 const request = require('supertest');
 const { expect } = require('chai');
 require('dotenv').config();
-const { obterToken }= require('../helpers/autenticacao')
+const { obterToken }= require('../helpers/autenticacao');
+const { beforeEach } = require('mocha');
 
 
 describe('Transferências', () => {
     describe('POST / transferencias', () => {
+
+        let token
+
+        beforeEach( async () => {
+            token = await obterToken('julio.lima', '123456')
+        })
+
         it('Deve retornar sucesso com 201 quando o valor da transferência for igual ou acima de R$ 10,00', async() => {
                     
-            const token = await obterToken('julio.lima', '123456')
-
             const resposta = await request(process.env.BASE_URL)
               
                 .post('/transferencias')
@@ -21,17 +27,13 @@ describe('Transferências', () => {
                     'valor': 11,
                     'token': ''
             })
-            expect(resposta.status).to.be.equal(201);
-
-            
+            expect(resposta.status).to.be.equal(201);           
 
         })
 
         
         it('Deve retornar falha com 422 quando o valor da transferência for abaixo de R$ 10,00', async() => {
            
-            const token = await obterToken('julio.lima', '123456')
-
             const resposta = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
